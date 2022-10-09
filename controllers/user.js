d5cf56b89c7c3f167f96cea8a37bbb08822073aa
Ricardo Melida <ricardomelida92@gmail.com>
2022-10-09 16:05:39 -0300

    Se agrega metodo para obtener usuario por UID
    Se ha agregado metodo al controlador y route para que pueda obtener
    un usuario de acuerdo al UID de dicho usuario. La misma se pasa como
    parametro por la URL.

---
+++ controllers/user.js
@@ -0,0 +1,14 @@
+const { authAdmin } = require("../services/firebase-admin-service");
+const getUser = (req, res) => {
+  const { uid } = req.params;
+  authAdmin
+    .getUser(uid)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch((err) => {
+      res.status(500).send({ err });
+    });
+};
+
+module.exports = { getUser };
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const bookRouter = require('./book');
 const authRouter = require('./auth');
+const userRoutes = require('./user');
 
 const router = express();
 
 router.use('/books', bookRouter);
 router.use('/auth', authRouter);
+router.use('/user', userRoutes);
 
 module.exports = router;
