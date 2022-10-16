bf858356bcce707a9e12b83b360f5dd403681fef
Ricardo Melida <ricardomelida92@gmail.com>
2022-10-15 22:40:29 -0300

    Se agrega envio de correo al 	ni

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 const { authAdmin } = require("../services/firebase-admin-service");
 const { auth } = require("../services/firebase-service");
-const { signInWithEmailAndPassword } = require("firebase/auth");
-const { async } = require("@firebase/util");
+const {
+  signInWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+} = require("firebase/auth");
 
 const signUp = (req, res) => {
   try {
@@ -31,15 +34,28 @@ const signIn = (req, res) => {
 
   if (email == null || password == null)
     return res.status(400).send({ message: "Algunos campos estan vacios" });
+
   signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) =>
-      res.status(200).send({ userData: userCredential.user })
-    )
+    .then((userCredential) => {
+      sendMailVerification(getAuth().currentUser)
+        .then(() => res.status(200).send({ userData: userCredential.user }))
+        .catch((errEmail) =>
+          res.status(400).send({
+            error: errEmail,
+            message: "Error al enviar mensaje de verificacion",
+          })
+        );
+    })
     .catch((error) =>
       res
         .status(400)
         .send({ error: error, message: "Email o Contraseña incorrecta" })
     );
+};
+
+const sendMailVerification = async (userRegisterd) => {
+  await sendEmailVerification(userRegisterd)
+    
 };
 
 const logOut = (req, res) => {
