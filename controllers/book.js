f9d2385966ce1862ec827fb0ae7a96420f4e7b6e
Ricardo Melida <ricardomelida92@gmail.com>
2022-10-23 22:18:58 -0300

    Se agrega metodo para subir nuevo libro en Firestore
    Se ha agregado el methodo setBook en el controlador book, esto con
    la finalidad de poder subir un nuevo libro en la BD.
    
    Todavia faltaria agregar a quien corresponde el libro que se sube,
    y tambien ver otros campos en el caso de que haga falta, esto segun
    documentacion.

--- controllers/book.js
+++ controllers/book.js
@@ -1,47 +1,61 @@
 const { firestoreAdmin } = require("../services/firebase-admin-service");
-const Book = {
-  getBooks: async (req, res) => {
-    try {
-      const querySnapshot = await firestoreAdmin.collection("book").get();
-      const books = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      return res.status(200).send({ books });
-    } catch (error) {
-      return res.status(500).send({ error });
-    }
-  },
+const { v4: uuid } = require("uuid");
 
-  getBookById: async (req, res) => {
-    const { uid } = req.params;
-
-    try {
-      const bookByUid = await firestoreAdmin.collection("book").doc(uid).get();
-
-      if (bookByUid.data() == null || bookByUid.data() == "")
-        return res.status(404).send({ data: "No Data" });
-
-      return res.status(200).send(bookByUid.data());
-    } catch (error) {
-      return res.status(500).send({ error: error });
-    }
-  },
-
-  getFavouriteBooks: (req, res) => {
-    res.send("getFavouriteBooks");
-  },
-
-  getMyBooks: (req, res) => {
-    res.send("getMyBooks");
-  },
-
-  setBook: (req, res) => {
-    const myFile = req.files;
-    console.log(myFile);
-    console.log(req.body);
-    res.send("setBook");
-  },
+//const Book = {
+const getBooks = async (req, res) => {
+  try {
+    const querySnapshot = await firestoreAdmin.collection("book").get();
+    const books = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return res.status(200).send({ books });
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
 
-module.exports = Book;
+const getBookById = async (req, res) => {
+  const { uid } = req.params;
+
+  try {
+    const bookByUid = await firestoreAdmin.collection("book").doc(uid).get();
+
+    if (bookByUid.data() == null || bookByUid.data() == "")
+      return res.status(404).send({ data: "No Data" });
+
+    return res.status(200).send(bookByUid.data());
+  } catch (error) {
+    return res.status(500).send({ error: error });
+  }
+};
+
+const getFavouriteBooks = (req, res) => {
+  res.send("getFavouriteBooks");
+};
+
+const getMyBooks = (req, res) => {
+  res.send("getMyBooks");
+};
+
+const setBook = async (req, res) => {
+  const bookData = req.body;
+
+  try {
+    const serverRespose = await firestoreAdmin
+      .collection("book")
+      .doc(uuid())
+      .create(bookData);
+    return res.status(200).send({ serverRespose });
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
+};
+
+module.exports = {
+  getBooks,
+  getBookById,
+  getFavouriteBooks,
+  getMyBooks,
+  setBook,
+};
--- routes/book.js
+++ routes/book.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const bookController = require("../controllers/book");
 const { isAuth } = require("../middlewares/userAutenticated");
 const router = express.Router();
+const {
+  getBooks,
+  getBookById,
+  getFavouriteBooks,
+  getMyBooks,
+  setBook,
+} = require("../controllers/book");
 
 const multipart = require("connect-multiparty");
 const mdUpload = multipart({ uploadDir: "./upload" });
 
-router.get("/", bookController.getBooks);
-router.get("/:uid", bookController.getBookById);
-router.get("/favourite/:id", bookController.getFavouriteBooks);
-router.get("/my/:id", bookController.getMyBooks);
-router.post("/", mdUpload, bookController.setBook);
+router.get("/", getBooks);
+router.get("/:uid", getBookById);
+router.get("/favourite/:id", getFavouriteBooks);
+router.get("/my/:id", getMyBooks);
+router.post("/set-book", mdUpload, setBook);
 
 module.exports = router;
